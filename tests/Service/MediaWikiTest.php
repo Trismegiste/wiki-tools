472b1c2f70ebca1f2960eefe36e0d1f7ea1af685
Trismegiste <flo@trismegiste.net>
2021-07-08 10:21:35 +0200

    unit test with mock http

--- tests/Service/MediaWikiTest.php
+++ tests/Service/MediaWikiTest.php
@@ -1,11 +1,43 @@
 <?php
 
-class MediaWikiTest extends PHPUnit\Framework\TestCase
+namespace App\Tests\Service;
+
+use App\Service\MediaWiki;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class MediaWikiTest extends TestCase
 {
 
+    protected $sut;
+    protected $network;
+
+    private function createFakeResponse()
+    {
+        $r = $this->createStub(ResponseInterface::class);
+        $r->method('getStatusCode')
+                ->willReturn(200);
+
+        return $r;
+    }
+
+    protected function setUp(): void
+    {
+        $this->network = $this->createStub(HttpClientInterface::class);
+        $this->sut = new MediaWiki($this->network, 'dummy.com');
+    }
+
+    /** @covers MediaWiki::searchPageFromCategory */
     public function testSearchPageFromCategory()
     {
-        $this->assertTrue(false);
+        $resp = $this->createFakeResponse();
+        $resp->method('getContent')
+                ->willReturn(json_encode(['query' => ['categorymembers' => ['entry']]]));
+        $this->network->method('request')
+                ->willReturn($resp);
+
+        $this->assertEquals(['entry'], $this->sut->searchPageFromCategory('yolo'));
     }
 
 }
